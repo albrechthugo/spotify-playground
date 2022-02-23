120894bb1cc3fd2421d1917891efd1d4a38715cc
Hugo <hugo.albrecht@picpay.com>
2022-02-23 19:52:28 -0300

    feat(auth): authenticate user and redirect to dashboard if has access token

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,43 @@
+import { CSSProperties, useEffect } from 'react'
+
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 
+import { Button } from '~/components'
+import { config } from '~/core/config'
+
+const styles: CSSProperties = {
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}
+
 const Home: NextPage = () => {
-  const { push } = useRouter()
+  const { push, query } = useRouter()
+  const { auth_base_url, client_id, redirect_uri, scopes } = config
+
+  useEffect(() => {
+    if (query?.code) push('/dashboard')
+  }, [push, query])
 
   return (
-    <>
-      <h1>authpage</h1>
-      <button onClick={() => push('dashboard')}>dashboard</button>
-    </>
+    <main style={styles}>
+      <Button
+        label="Entrar com Spotify"
+        onClick={() =>
+          push({
+            pathname: auth_base_url,
+            query: {
+              client_id,
+              response_type: 'code',
+              redirect_uri,
+              scope: scopes
+            }
+          })
+        }
+      />
+    </main>
   )
 }
 
