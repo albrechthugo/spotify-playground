881bd8d95cc88d57a4efffa78ae45d9cdf30ac2a
Hugo <albrechthugo4@gmail.com>
2022-03-30 22:15:32 -0300

    chore: add more props to config

--- src/types/config.ts
+++ src/types/config.ts
@@ -2,5 +2,8 @@ export type Config = {
   client_id: string
   redirect_uri: string
   auth_base_url: string
+  access_token_endpoint: string
+  api_base_url: string
+  spotify_base_url: string
   scopes: string
 }
