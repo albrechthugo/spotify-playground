878da13ae9839160cc95ebcaa295ebb49d6d8a65
Hugo <albrechthugo4@gmail.com>
2022-04-07 21:23:21 -0300

    feat(dashboard): fetch user top artists and show their names

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,32 @@
-import { Layout } from '~/components'
+import { GetServerSidePropsContext } from 'next'
 
-const Dashboard = () => <h1>dashboard</h1>
+import { Layout } from '~/components'
+import { config } from '~/core/config'
+import { Artist } from '~/core/entities'
+
+interface DashboardProps {
+  artists: Artist[]
+}
+
+export async function getServerSideProps({ req }: GetServerSidePropsContext) {
+  const response = await fetch(`${config.api_base_url}/user-top-artists`, {
+    headers: { token: req.cookies.token }
+  })
+
+  const {
+    data: { items }
+  } = await response.json()
+
+  return {
+    props: {
+      artists: items
+    }
+  }
+}
+
+const Dashboard = ({ artists }: DashboardProps) => {
+  return artists.map(({ id, name }) => <h1 key={id}>{name}</h1>)
+}
 
 Dashboard.layout = Layout
 
