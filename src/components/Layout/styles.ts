a8b3087e7b459884e25ec856bb7c5a6468ca36a5
Hugo <albrechthugo4@gmail.com>
2022-04-24 20:36:30 -0300

    fix(global-styles): adjust default layout to remove horizontal overflow

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import media from 'styled-media-query'
 
 export const Main = styled.main`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   display: grid;
 
