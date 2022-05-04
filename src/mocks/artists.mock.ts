081e85be13598c2e6dbae9fc33b7c23cf97ecdbb
Hugo <albrechthugo4@gmail.com>
2022-05-03 21:50:07 -0300

    wip(dashboard): create an artists mock

---
+++ src/mocks/artists.mock.ts
@@ -0,0 +1,85 @@
+import { Artist } from '~/core/entities'
+
+export const artistsMock: Artist[] = [
+  {
+    followers: {
+      total: 24169394
+    },
+    genres: ['arrocha', 'sertanejo'],
+    href: 'https://api.spotify.com/v1/artists/1yR65psqiazQpeM79CcGh8',
+    id: '1yR65psqiazQpeM79CcGh8',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab6761610000e5eb59035f0b5c852aabd865a080',
+        width: 640
+      },
+      {
+        height: 320,
+        url: 'https://i.scdn.co/image/ab6761610000517459035f0b5c852aabd865a080',
+        width: 320
+      },
+      {
+        height: 160,
+        url: 'https://i.scdn.co/image/ab6761610000f17859035f0b5c852aabd865a080',
+        width: 160
+      }
+    ],
+    name: 'Marília Mendonça',
+    popularity: 88
+  },
+  {
+    followers: {
+      total: 10494769
+    },
+    genres: ['sertanejo', 'sertanejo pop', 'sertanejo universitario'],
+    href: 'https://api.spotify.com/v1/artists/59jlthNnbmim5l9tmNA7se',
+    id: '59jlthNnbmim5l9tmNA7se',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab6761610000e5eb5c4fb6e84b60ed2ba517f815',
+        width: 640
+      },
+      {
+        height: 320,
+        url: 'https://i.scdn.co/image/ab676161000051745c4fb6e84b60ed2ba517f815',
+        width: 320
+      },
+      {
+        height: 160,
+        url: 'https://i.scdn.co/image/ab6761610000f1785c4fb6e84b60ed2ba517f815',
+        width: 160
+      }
+    ],
+    name: 'Maiara & Maraisa',
+    popularity: 82
+  },
+  {
+    followers: {
+      total: 2696320
+    },
+    genres: ['rap df'],
+    href: 'https://api.spotify.com/v1/artists/1xxS2WrLlgl9QNGEUCsbPG',
+    id: '1xxS2WrLlgl9QNGEUCsbPG',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab6761610000e5ebede4e3b946e1408cd4bb089e',
+        width: 640
+      },
+      {
+        height: 320,
+        url: 'https://i.scdn.co/image/ab67616100005174ede4e3b946e1408cd4bb089e',
+        width: 320
+      },
+      {
+        height: 160,
+        url: 'https://i.scdn.co/image/ab6761610000f178ede4e3b946e1408cd4bb089e',
+        width: 160
+      }
+    ],
+    name: 'Tribo da Periferia',
+    popularity: 69
+  }
+]
