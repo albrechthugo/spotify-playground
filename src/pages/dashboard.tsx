bf33dc5f5675487ac4dda20592448bac69ed1c26
Hugo <albrechthugo4@gmail.com>
2022-06-05 18:37:32 -0300

    feat: set user name in user context

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,31 +2,46 @@ import { GetServerSidePropsContext } from 'next'
 
 import { Layout } from '~/components'
 import { config } from '~/core/config'
+import { useUser } from '~/hooks'
 import { DashboardProps } from '~/templates'
 import DashboardTemplate from '~/templates/Dashboard'
 
 const QUERY_PARAMS = new URLSearchParams({ limit: '10' })
-const USER_TOP_ARTISTS_ENDPOINT = `${config.spotify_base_url}/me/top/artists?${QUERY_PARAMS}`
+const USER_ENDPOINT = `${config.spotify_base_url}/me`
+const USER_TOP_ARTISTS_ENDPOINT = `${USER_ENDPOINT}/top/artists?${QUERY_PARAMS}`
 
 export async function getServerSideProps({ req }: GetServerSidePropsContext) {
-  const response = await fetch(USER_TOP_ARTISTS_ENDPOINT, {
+  const options = {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${req.cookies.token}`,
       'Content-Type': 'application/json'
     }
-  })
+  }
 
-  const { items: artists } = await response.json()
+  const responses = await Promise.all([
+    fetch(USER_ENDPOINT, options),
+    fetch(USER_TOP_ARTISTS_ENDPOINT, options)
+  ])
+
+  const [{ display_name: user }, { items: artists }] = await Promise.all([
+    responses[0].json(),
+    responses[1].json()
+  ])
 
   return {
     props: {
-      artists
+      artists,
+      user
     }
   }
 }
 
-const Dashboard = ({ artists }: DashboardProps) => {
+const Dashboard = ({ artists, user }: DashboardProps & { user: string }) => {
+  const { setCurrentUser } = useUser()
+
+  setCurrentUser(user)
+
   return <DashboardTemplate artists={artists} />
 }
 
