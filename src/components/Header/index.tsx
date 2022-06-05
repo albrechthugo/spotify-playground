aaf08111d1ddf14af9f45941946320ba3382800b
Hugo <albrechthugo4@gmail.com>
2022-06-05 18:38:01 -0300

    feat: show user name in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
+import { useUser } from '~/hooks'
+
 import * as S from './styles'
 
 export const Header = () => {
+  const { user } = useUser()
+
+  const [firstName] = user.split(' ')
+
   return (
     <S.Header>
       <S.Title>Spotify Playground</S.Title>
-      <S.Username>Stephen Curry</S.Username>
+      <S.Username>Bem vindo, {firstName}</S.Username>
     </S.Header>
   )
 }
