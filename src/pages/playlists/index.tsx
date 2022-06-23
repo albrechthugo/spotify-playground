c8452069c4f044cec849ca383858988a2c162eb5
Hugo <albrechthugo4@gmail.com>
2022-06-23 16:40:31 -0300

    style: fix lint issues

--- src/pages/playlists/index.tsx
+++ src/pages/playlists/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { useCookies } from 'react-cookie'
 
 import { GetServerSidePropsContext } from 'next'
