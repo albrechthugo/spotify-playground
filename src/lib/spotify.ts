58a5493aa152cf62aa5aa95ecf356aec6b9d057f
Hugo <albrechthugo4@gmail.com>
2022-06-23 17:21:09 -0300

    refactor: move getUserInfo and getTopArtists to the spotify lib

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,6 +1,43 @@
 import { config } from '~/core/config'
+import { Artist, Playlist } from '~/core/entities'
 
-export const getPlaylists = async (token: string) => {
+const QUERY_PARAMS = new URLSearchParams({ limit: '10' })
+const USER_ENDPOINT = `${config.spotify_base_url}/me`
+const USER_TOP_ARTISTS_ENDPOINT = `${USER_ENDPOINT}/top/artists?${QUERY_PARAMS}`
+
+export const getUserInfo = async (token: string): Promise<string> => {
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const response = await fetch(USER_ENDPOINT, options)
+
+  const { display_name: user } = await response.json()
+
+  return user
+}
+
+export const getTopArtists = async (token: string): Promise<Artist[]> => {
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const response = await fetch(USER_TOP_ARTISTS_ENDPOINT, options)
+
+  const { items: artists } = await response.json()
+
+  return artists
+}
+
+export const getPlaylists = async (token: string): Promise<Playlist[]> => {
   const USER_PLAYLISTS_ENDOINT = `${config.spotify_base_url}/me/playlists`
   const options = {
     method: 'GET',
@@ -17,7 +54,10 @@ export const getPlaylists = async (token: string) => {
   return playlists
 }
 
-export const unfollowPlaylist = async (playlistId: string, token: string) => {
+export const unfollowPlaylist = async (
+  playlistId: string,
+  token: string
+): Promise<void> => {
   const UNFOLLOW_PLAYLIST_ENDPOINT = `${config.spotify_base_url}/playlists/${playlistId}/followers`
   const options: RequestInit = {
     method: 'DELETE',
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,33 +3,15 @@ import { useEffect } from 'react'
 import { GetServerSidePropsContext } from 'next'
 
 import { Layout } from '~/components'
-import { config } from '~/core/config'
 import { useUser } from '~/hooks'
+import { getTopArtists, getUserInfo } from '~/lib'
 import { DashboardProps } from '~/templates'
 import DashboardTemplate from '~/templates/Dashboard'
 
-const QUERY_PARAMS = new URLSearchParams({ limit: '10' })
-const USER_ENDPOINT = `${config.spotify_base_url}/me`
-const USER_TOP_ARTISTS_ENDPOINT = `${USER_ENDPOINT}/top/artists?${QUERY_PARAMS}`
-
 export async function getServerSideProps({ req }: GetServerSidePropsContext) {
-  const options = {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${req.cookies.token}`,
-      'Content-Type': 'application/json'
-    }
-  }
-
-  const responses = await Promise.all([
-    fetch(USER_ENDPOINT, options),
-    fetch(USER_TOP_ARTISTS_ENDPOINT, options)
-  ])
-
-  const [{ display_name: user }, { items: artists }] = await Promise.all([
-    responses[0].json(),
-    responses[1].json()
-  ])
+  const token = req.cookies.token
+  const user = await getUserInfo(token)
+  const artists = await getTopArtists(token)
 
   return {
     props: {
