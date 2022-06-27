6d6647b87e97543a34f53c770607b5fbfff0ab6f
Hugo <albrechthugo4@gmail.com>
2022-06-27 17:26:49 -0300

    remove all promises to test

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,19 +6,20 @@ import { Layout } from '~/components'
 import { Artist } from '~/core/entities'
 import { useUser } from '~/hooks'
 import { getUserInfo } from '~/lib'
+import { artistsMock } from '~/mocks'
 import { DashboardProps } from '~/templates'
 import DashboardTemplate from '~/templates/Dashboard'
 
 export async function getServerSideProps({ req }: GetServerSidePropsContext) {
   const token = req.cookies.token
 
-  const responses = await Promise.all([
-    getUserInfo(token)
-    // getTopArtists(token)
-  ])
+  // const responses = await Promise.all([
+  //   getUserInfo(token)
+  //   // getTopArtists(token)
+  // ])
 
-  const user = responses[0]
-  const artists = [] as Artist[]
+  const user = 'Teste'
+  const artists = artistsMock
 
   return {
     props: {
