8168c2082b896509ef5f6c62815aa7a68109bd0a
Hugo <albrechthugo4@gmail.com>
2022-06-30 00:02:47 -0300

    feat: create an api route to fetch user playlists

--- src/hooks/usePlaylists.ts
+++ src/hooks/usePlaylists.ts
@@ -1,16 +1,17 @@
 import create from 'zustand'
 
-import { getPlaylists } from '~/lib'
 import { Playlist } from '~/types'
 
 interface PlaylistStoreProps {
   playlists: Playlist[]
-  fetch: (token: string) => Promise<void>
+  fetch: () => Promise<void>
 }
 
 export const usePlaylists = create<PlaylistStoreProps>((set) => ({
   playlists: [],
-  fetch: async (token: string) => {
-    set({ playlists: await getPlaylists(token) })
+  fetch: async () => {
+    set({
+      playlists: await fetch('api/me/playlists').then((res) => res.json())
+    })
   }
 }))
