f90d230a25fc435cda4453fe37c226a4a0d11e16
Hugo <albrechthugo4@gmail.com>
2022-07-03 21:23:37 -0300

    perf: use on unfollow playlist button

--- src/components/UnfollowPlaylistButton/index.tsx
+++ src/components/UnfollowPlaylistButton/index.tsx
@@ -1,20 +1,20 @@
+import { memo } from 'react'
+
 import { Close } from '@mui/icons-material'
 import { IconButton } from '@mui/material'
 
 import * as S from './styles'
 
-export const UnfollowPlaylistButton = ({
-  handleClick
-}: {
-  handleClick: () => void
-}) => {
-  return (
-    <>
-      <S.Container onClick={handleClick}>
-        <IconButton aria-label="Deixar de seguir playlist">
-          <Close color="primary" fontSize="large" />
-        </IconButton>
-      </S.Container>
-    </>
-  )
-}
+export const UnfollowPlaylistButton = memo(
+  ({ handleClick }: { handleClick: () => void }) => {
+    return (
+      <>
+        <S.Container onClick={handleClick}>
+          <IconButton aria-label="Deixar de seguir playlist">
+            <Close color="primary" fontSize="large" />
+          </IconButton>
+        </S.Container>
+      </>
+    )
+  }
+)
